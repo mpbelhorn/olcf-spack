5fa1f3891ce2d45cdd97227faac0e24a8879d27c
Matt Belhorn <belhornmp@ornl.gov>
2021-03-19 09:52:06 -0400

    Adds Spark v3.1.1

--- var/spack/repos/builtin/packages/spark/package.py
+++ var/spack/repos/builtin/packages/spark/package.py
@@ -22,6 +22,7 @@ class Spark(Package):
     depends_on('java', type=('build', 'run'))
     depends_on('hadoop', when='+hadoop', type=('build', 'run'))
 
+    version('3.1.1', sha256='2d16f527016993e69a7c801233e661a170e3099ae2d9b950f7457729d63062f4')
     version('2.3.0', sha256='a7e29e78bd43aa6d137f0bb0afd54a3017865d471456c6d436ae79475bbeb161')
     version('2.1.0', sha256='3ca4ecb0eb9a00de5099cc2564ed957433a2d15d9d645a60470324621853c5ae')
     version('2.0.2', sha256='122ec1af0fcb23c0345f20f77d33cf378422ffe966efe4b9ef90e55cf7a46a3c')
