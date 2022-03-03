deb635b970865eac3eea63b54b34d5c37bb06397
Matt Belhorn <belhornmp@ornl.gov>
2022-03-03 18:19:20 -0500

    Skips patching missing libiberty config files.
    The libiberty subdirectory does not contain its own config files and
    Spack should not attempt to patch them.

--- var/spack/repos/builtin/packages/libiberty/package.py
+++ var/spack/repos/builtin/packages/libiberty/package.py
@@ -38,6 +38,7 @@ class Libiberty(AutotoolsPackage, GNUMirrorPackage):
 
     # Configure and build just libiberty.
     configure_directory = 'libiberty'
+    patch_config_files = False
 
     # Set default cflags (-g -O2), add -fPIC if requested, and move to
     # the configure line.
