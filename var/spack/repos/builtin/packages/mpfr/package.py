867977594b03ef475b8ccaf6e6df28de63cd8186
Matt Belhorn <belhornmp@ornl.gov>
2022-03-03 16:29:41 -0500

    Updates adds missing autoconf macros to mpfr build-time and runtime environments
    Additional autoconf macros are needed for v4.0.2 which are provided by
    the autoconf-archive package. This commit ensures the autoconf-archive
    macros are available at buildtime for mpfr dependents and at mpfr
    runtime.

--- var/spack/repos/builtin/packages/mpfr/package.py
+++ var/spack/repos/builtin/packages/mpfr/package.py
@@ -31,7 +31,7 @@ class Mpfr(AutotoolsPackage, GNUMirrorPackage):
     depends_on('automake', type='build')
     depends_on('libtool', type='build')
     depends_on('m4', type='build')
-    depends_on('autoconf-archive', when='@4.0.2:', type='build')
+    depends_on('autoconf-archive', when='@4.0.2:', type=('build', 'run'))
     depends_on('texinfo', when='@4.1.0', type='build')
 
     variant('libs', default='shared,static', values=('shared', 'static'),
