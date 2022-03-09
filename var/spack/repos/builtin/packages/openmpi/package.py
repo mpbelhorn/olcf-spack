3a534acbfbf7129b7111c54fb3c9741e87aeaccc
Matt Belhorn <belhornmp@ornl.gov>
2022-03-09 10:52:51 -0500

    Makes OMPI fabrics a sticky variant

--- var/spack/repos/builtin/packages/openmpi/package.py
+++ var/spack/repos/builtin/packages/openmpi/package.py
@@ -212,6 +212,7 @@ class Openmpi(AutotoolsPackage, CudaPackage):
         ).with_non_feature_values('auto', 'none'),
         description="List of fabrics that are enabled; "
                     "'auto' lets openmpi determine",
+        sticky=True,
     )
 
     variant(
